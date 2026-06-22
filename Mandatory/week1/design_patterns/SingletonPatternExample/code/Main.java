36c66e7462712a6588e098c5de7ba2777eaf2c47
Ranjani <ranjaniraj0715@gmail.com>
2026-06-22 13:03:35 +0530

    Added Week1 Singleton Pattern Example

---
+++ Mandatory/week1/design_patterns/SingletonPatternExample/code/Main.java
@@ -0,0 +1,22 @@
+package SingletonPatternExample;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Get Logger instances
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        // Use logger
+        logger1.log("Application started.");
+        logger2.log("Singleton pattern implemented.");
+
+        // Verify same instance
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger instance exists.");
+        } else {
+            System.out.println("Multiple instances created.");
+        }
+    }
+}
